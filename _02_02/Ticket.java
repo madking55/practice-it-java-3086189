abb1c8b39b48b6968c75a266709e2ae1933074fd
Kinga <37161970+madking55@users.noreply.github.com>
2024-02-10 10:48:46 +0000

    Finish Classess and Objects chapter

---
+++ _02_02/Ticket.java
@@ -0,0 +1,19 @@
+// 1. Create a class called Ticket 
+// 2. Give the class an empty constructor 
+// 3. Create 3 private field variables in the class: 
+// - A field of type String called destination
+// - A field called price of type double
+// - A field called isReturn of type boolean
+
+package _02_02;
+
+public class Ticket {
+
+  public Ticket() {
+  }
+
+  private String destination;
+  private double price;
+  private boolean isReturn;
+
+}
